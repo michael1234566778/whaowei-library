df66d94b8812ccb2172471cb9a38e4fba51f79fc
Haowei W <1825980932@qq.com>
2024-08-06 15:14:07 +1000

    week3

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <script setup>
-import Json from './components/JSON.vue'
+//import Json from './components/JSON.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <Json />
+    <Form />
   </main>
 </template>
 
